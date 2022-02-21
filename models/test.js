464566e6b74b71edd0e6192dffab4640cae91110
YOGESH YADAV <70858211+yogeshrdr@users.noreply.github.com>
2022-02-21 21:42:29 +0530

    Update test.js

--- models/test.js
+++ models/test.js
@@ -2,33 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 require('dotenv').config();
 
-// const optionSchema = new Schema({
-//     option:{
-//         type: String,
-//         required: 'mcqtitle is Required'
-//     },
-//     status:{
-//         type: Boolean,
-//         default: false
-//     },
-// });
+const optionSchema = new Schema({
+    option:{
+        type: String,
+        required: 'mcqtitle is Required'
+    },
+    status:{
+        type: Boolean,
+        default: false
+    },
+});
 
-// const QuestionSchema = new Schema({
-//     questionTitle:{
-//         type: String,
-//         required: 'title is Required'
-//     },
-//     mcqType:{
-//         type: Boolean,
-//         default: false
-//     },
-//     questionMarks:{
-//         type: Number,
-//     },
-//     options:[{
-//         type: optionSchema
-//     }]
-// });
+const QuestionSchema = new Schema({
+    questionTitle:{
+        type: String,
+        required: 'title is Required'
+    },
+    mcqType:{
+        type: Boolean,
+        default: false
+    },
+    questionMarks:{
+        type: Number,
+    },
+    options:[{
+        type: optionSchema
+    }]
+});
 
 
 const testSchema = new Schema({
@@ -36,39 +36,39 @@ const testSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'teacher'
     },
-//     title:{
-//         type: String
-//     },
-//     startTime:{
-//         type: Number
-//     },
-//     endTime:{
-//         type: Number
-//     },
-//     branch:{
-//         type: String
-//     },
-//     section:{
-//         type: String,
-//         default: "null"
-//     },
-//     isBranch:{
-//         type: Boolean,
-//         default: false
-//     },
-//     totalMarks:{
-//         type: Number
-//     },
-//     isDuration:{
-//         type: Boolean,
-//         default: false
-//     },
-//     duration:{
-//         type: Number
-//     },
-//     questions:[{
-//         type: QuestionSchema
-//     }]
+    title:{
+        type: String
+    },
+    startTime:{
+        type: Number
+    },
+    endTime:{
+        type: Number
+    },
+    branch:{
+        type: String
+    },
+    section:{
+        type: String,
+        default: "null"
+    },
+    isBranch:{
+        type: Boolean,
+        default: false
+    },
+    totalMarks:{
+        type: Number
+    },
+    isDuration:{
+        type: Boolean,
+        default: false
+    },
+    duration:{
+        type: Number
+    },
+    questions:[{
+        type: QuestionSchema
+    }]
 }, { timestamps: true });
 
 
